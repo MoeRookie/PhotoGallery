daf05932d1d749fcd2171b267e8c53cf54e516f0
MoeRookie <MoeRookie@163.com>
2018-09-21 09:54:06 +0800

    使用AsyncTask,创建后台线程并测试网络连接的代码

--- app/src/main/java/com/ghsoft/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/ghsoft/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,21 @@
 package com.ghsoft.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment{
+    private static final String TAG = "PhotoGalleryFragment";
 
     private RecyclerView mPhotoRecyclerView;
 
@@ -19,6 +24,7 @@ public class PhotoGalleryFragment extends Fragment{
         super.onCreate(savedInstanceState);
         // 保留fragment
         setRetainInstance(true);
+        new FetchItemTask().execute();
     }
 
     public static Fragment newInstance() {
@@ -33,5 +39,19 @@ public class PhotoGalleryFragment extends Fragment{
         mPhotoRecyclerView = view.findViewById(R.id.fragment_photo_gallery_recycler_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
         return view;
+    }
+    private class FetchItemTask extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                String result = new FlickrFetchr()
+                        .getUrlString("https://www.baidu.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
