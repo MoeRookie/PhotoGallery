dadd4baad2deea8c2e2e8fc865c17f829760fe5f
MoeRookie <MoeRookie@163.com>
2018-11-11 22:20:51 +0800

    关联使用反馈handler

--- app/src/main/java/com/ghsoft/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/ghsoft/photogallery/PhotoGalleryFragment.java
@@ -1,8 +1,11 @@
 package com.ghsoft.photogallery;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -33,7 +36,19 @@ public class PhotoGalleryFragment extends Fragment{
         setRetainInstance(true);
         new FetchItemTask().execute();
 
-        mThumbnailDownloader = new ThumbnailDownloader<>();
+        Handler responseHandler = new Handler();
+        mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
+        mThumbnailDownloader.setThumbnailDownloadListener(
+                new ThumbnailDownloader.ThumbnailDownloadListener<PhotoHolder>() {
+                    @Override
+                    public void onThumbnailDownloader(PhotoHolder target, Bitmap thumbnail) {
+                        // bitmap -> drawable
+                        BitmapDrawable drawable = new BitmapDrawable(getResources(), thumbnail);
+                        // pH绑定drawable
+                        target.bindDrawable(drawable);
+                    }
+                }
+        );
         mThumbnailDownloader.start();
         mThumbnailDownloader.getLooper();
         Log.i(TAG, "Background thread started.");
